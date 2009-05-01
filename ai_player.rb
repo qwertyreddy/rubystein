7c2ceeaaea4e417011e6ac86820ca2c1b762ff5d
Hongli Lai (Phusion) <hongli@phusion.nl>
2009-05-01 16:59:10 +0200

    Make Rock-Zed turn into a magic pony.

--- ai_player.rb
+++ ai_player.rb
@@ -501,7 +501,7 @@ class Zed < Enemy
 end
 
 class TrueZed < Enemy
-  def initialize(window, map, x, y, death_sound = 'mein_spagetthicode.wav', firing_sound = 'test_all_the_effing_time_is_lame.mp3', kill_score = 10000, step_size = 3, animation_interval = 0.2)
+  def initialize(window, map, x, y, death_sound = 'omgponies.mp3', firing_sound = 'test_all_the_effing_time_is_lame.mp3', kill_score = 10000, step_size = 3, animation_interval = 0.2)
     sprites = {
       :idle    => ['rockzed.png'],
       :walking => ['rockzed_moving.png', 'rockzed_moving2.png'],
@@ -509,8 +509,7 @@ class TrueZed < Enemy
                    'rockzed_attacking4.png', 'rockzed_attacking5.png', 'rockzed_attacking6.png',
                    'rockzed_attacking7.png', 'rockzed_attacking8.png', 'rockzed_attacking9.png'],
       :damaged => ['rockzed_damaged.png'],
-      :dead    => ['rockzed_dead.png', 'rockzed_dead2.png', 'rockzed_dead4.png', 'rockzed_dead5.png',
-                   'rockzed_dead6.png']
+      :dead    => ['magic_pony.png']
     }
     
     @name = "True Zed Shaw"
